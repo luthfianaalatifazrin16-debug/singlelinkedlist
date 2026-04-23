61edd3a69177a5acc9559fb4d7c1738a3f58e573
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-04-23 19:37:42 +0700

    int main

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -122,20 +122,3 @@ int main()
     char ch;
 
     do
-    {
-        cout << "\nMenu" << endl;
-        cout << "1. Menambah data ke dalam list" << endl;
-        cout << "2. Menghapus data dari dalam list" << endl;
-        cout << "3. Menampilkan semua data didalam list" << endl;
-        cout << "4. Mencari data dalam list" << endl;
-        cout << "5. Keluar" << endl;
-
-        cout << "\nMasukkan pilihan (1-5): ";
-        cin >> ch;
-
-        switch (ch)
-        {
-        case '1':
-            mhs.addNode();
-            break;
-        }
